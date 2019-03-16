d994760f0e28235fe875b18c275db5a62a2e9f90
Kerben Bermingham <b.kerben2@gmail.com>
2019-03-16 04:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  
+  }
+  if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (distance > 2500){
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(NYC){
+  if (NYC == NYC){
+    return 'Ok, sounds good.'
+}
+}
